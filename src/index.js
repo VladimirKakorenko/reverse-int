141ebe2d2e9f005203231cb79b8f4f9c0c444f99
Vladimir <vova3.4.99@gmail.com>
2022-02-15 14:55:47 +0300

    create algorithm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0){
+        n = n * (-1)
+    }
+    let string = n.toString();
+    let arr = string.split('');
+    let reverseArr = arr.reverse();
+    let reverseString = reverseArr.join('');
+    return Number(reverseString);
 }
